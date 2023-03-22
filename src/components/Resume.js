348ef52e80320c3ddfc3415e2cb1c24c3c3b80dc
Tyler Hall <thall2980@gmail.com>
2023-03-21 21:01:49 -0400

    added resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+
+
+
 
 const Resume = () => {
+
+  const [numPage, setNumPages] = useState(null)
+  const [pageNumber, setPageNumber] = useState(1)
+  const [file, setFile] = useState('../NO PHONE NUMBER Tyler_Hall_Resume_.pdf')
+
+  function onDocumentLoadSuccess({numPages}) {
+    setNumPages(numPage)
+    setNumPages(1)
+  }
   return (
-    <div>Resume</div>
+    <div>
+      <Document file ={file} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page height="1000" width="1000" pageNumber={pageNumber} renderTextLayer={false} />
+      </Document>
+    </div>
   )
 }
 
