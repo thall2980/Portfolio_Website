45b8566f28cfe407be5e14cc275cba908c42a3b2
Tyler Hall <thall2980@gmail.com>
2023-03-22 21:50:25 -0400

    about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div>
+      <h1>Tyler Hall</h1>
+      <h2>Full Stack Web Developer</h2>
+      <p>Hello there! My name is Tyler Hall, and I am a Full Stack Web Developer based in Pittsburgh, PA. Before entering the tech world, I attended the University of Pittsburgh and earned a Bachelor of Science degree in Biological Sciences.  I also worked as a Patient Care Technician at the University of Pittsburgh Medical Center Presbyterian Hospital on a physical rehbailition unit. After realizing my true passion for programming and web development, I attended the Flatiron School Software Engineering program where I honed my skills in Ruby on Rails and React.js programming.  The fruits of my labor are this website and the technical projects on display! I am currently looking for my first position in the field in order to demonstrate my skills and grow as a web developer.
+
+      Thank you for visiting my portfolio website, and please feel free to connect with me on LinkedIn or via email.
+      </p>
+    </div>
   )
 }
 
