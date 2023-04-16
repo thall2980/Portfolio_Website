89c979673daec68f690cab991b9342892dc0fb41
Tyler Hall <thall2980@gmail.com>
2023-04-15 21:22:52 -0400

    a tag chance

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
-import { TfiSave } from 'react-icons/tfi';
+import React from "react";
+import { useState } from "react";
+import { TfiSave } from "react-icons/tfi";
 // import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
-
-
-
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+  const [numPage, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [file, setFile] = useState("../NO PHONE NUMBER Tyler_Hall_Resume_.pdf");
 
-  const [numPage, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-  const [file, setFile] = useState('../NO PHONE NUMBER Tyler_Hall_Resume_.pdf')
-
-  function onDocumentLoadSuccess({numPages}) {
-    setNumPages(numPage)
-    setNumPages(1)
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPage);
+    setNumPages(1);
   }
   return (
     <div classname="h-full">
-      <div style={{textAlign:'center'}}>
-      <a href={file} className="w-full text-5xl"><TfiSave/></a>
+      <div>
+        <a href={file} className="text-5xl">
+          <TfiSave />
+        </a>
       </div>
-      <Document file ={file} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page   pageNumber={pageNumber} renderTextLayer={false} />
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} renderTextLayer={false} />
       </Document>
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
